2a8adc3809578b4b31aa1d414090c6c53ebd6b24
EmmanuelNoel <jahounoel@gmail.com>
2024-06-05 23:29:25 +0100

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const bodyParser = require('body-parser');
+const validUrl = require('valid-url');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -17,6 +23,25 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+let urlDatabase = {};
+let urlCounter = 1;
+
+// Route pour raccourcir une URL
+app.post('/api/shorturl', (req, res) => {
+  const original_url = req.body.url;
+
+  // Valider l'URL
+  if (!validUrl.isUri(original_url)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // Stocker l'URL dans la base de données simulée
+  const short_url = urlCounter++;
+  urlDatabase[short_url] = original_url;
+
+  res.json({ original_url, short_url });
 });
 
 app.listen(port, function() {
